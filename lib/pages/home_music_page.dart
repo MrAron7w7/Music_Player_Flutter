705683bb4627ff0d9e685a6f803c7ab15c12d8f0
AronDev <magallanestorresaron@gmail.com>
2023-11-05 00:59:20 -0500

    error

--- lib/pages/home_music_page.dart
+++ lib/pages/home_music_page.dart
@@ -28,36 +28,56 @@ class _HomeMusicPageState extends State<HomeMusicPage> {
           const SizedBox(width: 20),
         ],
       ),
-      body: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context, index) {
-          return const ListTile(
-            leading: Icon(
-              Icons.music_note,
-              color: primary,
-            ),
-            title: Text(
-              'Song Name',
-              style: TextStyle(
-                color: white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            subtitle: Text(
-              'Artist Name',
-              style: TextStyle(
-                color: white,
-                overflow: TextOverflow.ellipsis,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            trailing: Icon(
-              Icons.more_horiz,
-              color: primary,
-            ),
+      body: FutureBuilder<List<SongModel>>(
+        future: _audioQuery.querySongs(
+          sortType: null,
+          orderType: OrderType.ASC_OR_SMALLER,
+          uriType: UriType.EXTERNAL,
+          ignoreCase: true,
+        ),
+        builder: (context, item) {
+          if (item.data == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (item.data!.isEmpty) {
+            return const Center(
+              child: Text('No songs found'),
+            );
+          }
+          return ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return const ListTile(
+                leading: Icon(
+                  Icons.music_note,
+                  color: primary,
+                ),
+                title: Text(
+                  'Song Name',
+                  style: TextStyle(
+                    color: white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                subtitle: Text(
+                  'Artist Name',
+                  style: TextStyle(
+                    color: white,
+                    overflow: TextOverflow.ellipsis,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.more_horiz,
+                  color: primary,
+                ),
+              );
+            },
           );
         },
       ),
